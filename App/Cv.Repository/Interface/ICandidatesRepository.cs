a3ae0fa50fccf9dc79d05e159cafd43d7d0d55ed
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-02-11 22:35:17 -0300

    Update ICandidatesRepository.cs

--- App/Cv.Repository/Interface/ICandidatesRepository.cs
+++ App/Cv.Repository/Interface/ICandidatesRepository.cs
@@ -1,9 +1,13 @@
 ﻿using Cv.Models;
+using System.Collections.Generic;
 
 namespace Cv.Repository.Interface
 {
     public interface ICandidatesRepository
     {
         void Insert(CandidateModel candidated);
+        bool Update(CandidateModel candidate);
+        bool Delete(string id);
+        IList<CandidateModel> GetAllByCompanyId(string companyId);
     }
 }
