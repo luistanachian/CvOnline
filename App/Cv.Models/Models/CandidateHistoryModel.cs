681faf0ae13f34338a73df4d2da0129550b2afa6
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-02-18 14:04:30 -0300

    update

---
+++ App/Cv.Models/Models/CandidateHistoryModel.cs
@@ -0,0 +1,15 @@
+﻿using Cv.Models.Models.Items;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Cv.Models.Models
+{
+    public class CandidateHistoryModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string CandidateId { get; set; }
+        public List<EventItem> History { get; set; }
+    }
+}
---
+++ App/Cv.Models/Models/Items/EventItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cv.Models.Models.Items
+{
+    public class EventItem
+    {
+        public string UserId { get; set; }
+        public string Event { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
