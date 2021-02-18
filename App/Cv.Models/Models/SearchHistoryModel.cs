69c9c4c16f830a8685e52b48936759adeab85aea
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-02-18 14:32:54 -0300

    update

---
+++ App/Cv.Models/Models/SearchHistoryModel.cs
@@ -0,0 +1,15 @@
+﻿using Cv.Models.Items;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Cv.Models.Models
+{
+    class SearchHistoryModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string SearchId { get; set; }
+        public List<EventItem> History { get; set; }
+    }
+}
