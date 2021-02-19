c0918b483883a3bf3060b65a6f9356520f401b2f
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-02-18 21:49:36 -0300

    update

---
+++ App/Cv.Dao/Class/CandidateHistoryBusiness.cs
@@ -0,0 +1,13 @@
+﻿using Cv.Dao.Base.Class;
+using Cv.Dao.Interface;
+using Cv.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cv.Dao.Class
+{
+    public class CandidateHistoryBusiness : BaseDao<CandidateHistoryModel>, ICandidateHistoryDao
+    {
+    }
+}
---
+++ App/Cv.Dao/Interface/ICandidateHistoryDao.cs
@@ -0,0 +1,15 @@
+﻿using Cv.Dao.Base.Interface;
+using Cv.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cv.Dao.Interface
+{
+    public interface ICandidateHistoryDao :
+        IGetByDao<CandidateHistoryModel>,
+        IInsertDao<CandidateHistoryModel>,
+        IDeleteDao<CandidateHistoryModel>
+    {
+    }
+}
