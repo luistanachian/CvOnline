42402ffe036b349246beb60fc8a0f4bf3819cf75
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-03-03 20:35:47 -0300

    update

---
+++ App/Cv.Repository/Class/CompaniesStatisticsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Cv.Dao.Interface;
+using Cv.Models;
+using Cv.Repository.Interface;
+
+namespace Cv.Repository.Class
+{
+    public sealed class CompaniesStatisticsRepository : ICompaniesStatisticsRepository
+    {
+        private readonly ICompaniesStatisticsDao companiesStatisticsDao;
+        public CompaniesStatisticsRepository(ICompaniesStatisticsDao companiesStatisticsDao)
+        {
+            this.companiesStatisticsDao = companiesStatisticsDao;
+        }
+        public void Insert(CompanyStatisticsModel entity) => companiesStatisticsDao.Insert(entity);
+
+        public bool Replace(CompanyStatisticsModel entity) =>
+            companiesStatisticsDao.Replace(c => c.CompanyId == entity.CompanyId, entity) > 0;
+
+        public bool Delete(string id) =>
+            companiesStatisticsDao.Delete(c => c.CompanyId == id) > 0;
+
+        public CompanyStatisticsModel GetBy(string companyId) =>
+            companiesStatisticsDao.GetOneByFunc(c => c.CompanyId == companyId);
+
+    }
+}
---
+++ App/Cv.Repository/Interface/ICompaniesStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cv.Models;
+
+namespace Cv.Repository.Interface
+{
+    public interface ICompaniesStatisticsRepository
+    {
+        void Insert(CompanyStatisticsModel entity);
+        public bool Replace(CompanyStatisticsModel entity);
+        public bool Delete(string id);
+        public CompanyStatisticsModel GetBy(string companyId);
+    }
+}
