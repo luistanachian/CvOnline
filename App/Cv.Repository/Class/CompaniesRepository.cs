d26e045aeb1b2eac43d7fd8c4eade8bd0b8f1544
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-03-02 21:40:04 -0300

    update

---
+++ App/Cv.Repository/Class/CompaniesRepository.cs
@@ -0,0 +1,72 @@
+﻿using Cv.Dao.Interface;
+using Cv.Models;
+using Cv.Repository.Interface;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cv.Repository.Class
+{
+    public sealed class CompaniesRepository : ICompaniesRepository
+    {
+        private readonly ICompaniesDao companiesDao;
+        public CompaniesRepository(ICompaniesDao companiesDao)
+        {
+            this.companiesDao = companiesDao;
+        }
+
+        public bool Insert(CompanyModel entity)
+        {
+            try
+            {
+                companiesDao.Insert(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+                //TODO loguear
+                return false;
+            }
+        }
+
+        public bool Replace(CompanyModel entity)
+        {
+            try
+            {
+                return companiesDao.Replace(c => c.CompanyId == entity.CompanyId, entity) > 0;
+            }
+            catch (Exception)
+            {
+                //TODO loguear
+                return false;
+            }
+        }
+
+        public bool Delete(string id)
+        {
+            try
+            {
+                return companiesDao.Delete(c => c.CompanyId == id) > 0;
+            }
+            catch (Exception)
+            {
+                //TODO loguear
+                return false;
+            }
+        }
+
+        public CompanyModel GetBy(string id)
+        {
+            try
+            {
+                return companiesDao.GetOneByFunc(c => c.CompanyId == id);
+            }
+            catch (Exception)
+            {
+                //TODO loguear
+                return null;
+            }
+        }
+    }
+}
---
+++ App/Cv.Repository/Interface/ICompaniesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cv.Models;
+
+namespace Cv.Repository.Interface
+{
+    public interface ICompaniesRepository
+    {
+        bool Insert(CompanyModel entity);
+        bool Replace(CompanyModel entity);
+        bool Delete(string id);
+        CompanyModel GetBy(string id);
+    }
+}
