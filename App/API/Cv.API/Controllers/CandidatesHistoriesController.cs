1ce4c91ff628b0e744fe35120ed5690d4e232f8e
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-03-11 16:25:04 -0300

    Create CandidatesHistoriesController.cs

---
+++ App/API/Cv.API/Controllers/CandidatesHistoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Cv.Business.Interface;
+using Cv.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cv.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CandidatesHistoriesController : ControllerBase
+    {
+        private readonly ICandidatesHistoriesBusiness candidatesHistoriesBusiness;
+        public CandidatesHistoriesController(ICandidatesHistoriesBusiness candidatesHistoriesBusiness)
+        {
+            this.candidatesHistoriesBusiness = candidatesHistoriesBusiness;
+        }
+
+
+        [HttpGet]
+        [Route("{candidateId}")]
+        public async Task<CandidateHistoryModel> Get(string candidateId) =>
+            await candidatesHistoriesBusiness.GetBy(candidateId);
+
+    }
+}
